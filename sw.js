751a922e43fce85f03a6aeca405990ff2690f037
SpencerDJHS <135638618+SpencerDJHS@users.noreply.github.com>
2026-09-15 14:16:29 -0400

    Update CACHE_VERSION to 'esb-v81' in sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Update CACHE_VERSION when deploying changes.
 // ============================================
 
-const CACHE_VERSION = 'esb-v80';
+const CACHE_VERSION = 'esb-v81';
 
 const EXTERNAL_SCRIPTS = [
     'https://unpkg.com/dexie@4.0.8/dist/dexie.js',
